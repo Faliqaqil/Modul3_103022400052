5748ca0dc52aa226bdb1274de8efe8a2529cbae8
Faliqaqil <faliq.aqil.ra@gmail.com>
2026-03-10 07:20:44 +0700

    Menambahkan GUI suhu

--- Modul3_103022400052/Form1.Designer.cs
+++ Modul3_103022400052/Form1.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            cbSatuanAwal = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // cbSatuanAwal
+            // 
+            cbSatuanAwal.FormattingEnabled = true;
+            cbSatuanAwal.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            cbSatuanAwal.Location = new Point(132, 127);
+            cbSatuanAwal.Name = "cbSatuanAwal";
+            cbSatuanAwal.Size = new Size(112, 33);
+            cbSatuanAwal.TabIndex = 0;
+            cbSatuanAwal.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            comboBox2.Location = new Point(132, 238);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(112, 33);
+            comboBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(333, 129);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(198, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(593, 129);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 3;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(135, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 25);
+            label1.TabIndex = 4;
+            label1.Text = "Satuan Awal";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(132, 201);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 25);
+            label2.TabIndex = 5;
+            label2.Text = "Satuan Akhir";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(333, 240);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(198, 31);
+            textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(384, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 25);
+            label3.TabIndex = 7;
+            label3.Text = "Nilai Awal";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(381, 201);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 25);
+            label4.TabIndex = 8;
+            label4.Text = "Nilai Akhir";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(cbSatuanAwal);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox cbSatuanAwal;
+        private ComboBox comboBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
     }
 }
